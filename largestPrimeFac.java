966928fa6c75721e09f8380b6e92167fedd57f13
satvikDesktop <satviksingh2304@gmail.com>
2022-01-20 19:30:13 +0530

    problem 3

---
+++ largestPrimeFac.java
@@ -0,0 +1,21 @@
+public class largestPrimeFac {
+   public static void main (String[]args) {
+       System.out.println(MaxPrime());
+   }
+    public static long MaxPrime(){
+    long n=600851475143L;
+    long div=2;
+    long maxFact=0;
+    while(n!=0) {
+        if(n % div !=0)
+           div = div + 1;
+        else {
+           maxFact = n;
+           n = n / div;
+           if(n == 1) 
+             break;
+        }
+      }
+      return maxFact;
+   }
+}
